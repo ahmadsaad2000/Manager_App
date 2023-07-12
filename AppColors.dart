bd0bc55bf29bb8019059570d4d57fa8d8311f2cf
ahmadsaad2000 <95399451+ahmadsaad2000@users.noreply.github.com>
2023-07-12 15:42:00 +0300

    Add files via upload

---
+++ AppColors.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color mainColor = Color(0xFF1F1F1F);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color grayColor = Color(0xFF525050);
+  static const Color backColor = Color(0xFFF3EFEF);
+  static const Color redColor = Color(0xFFFD0404);
+}
---
+++ AppStyle.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+// import 'package:flutter/material.dart';
+// import '';
+
+// class {
+// TextStyle Googles =GoogleFonts.Google 
+
+// }
